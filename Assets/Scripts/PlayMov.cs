e9f1d2cd611e18d693ed2a80795beb0f3b69c789
Alan <664837069@qq.com>
2016-12-20 17:41:09 +0800

    成功添加视频，为以后实现电视开关进行初步demo测试

---
+++ Assets/Scripts/PlayMov.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMov : MonoBehaviour
+{
+
+    //电影纹理
+    public MovieTexture movTexture;
+    public AudioSource audioSource;
+
+
+    void Start()
+    {
+       
+
+
+    }
+
+    
+    void OnGUI()
+    {
+        if (!movTexture.isPlaying)
+        {
+            movTexture.Play();
+        }
+        if(Input.GetKeyDown(KeyCode.A)){
+
+                                        //设置当前对象的主纹理为电影纹理
+            GetComponent<Renderer>().material.mainTexture = movTexture;
+            //设置电影纹理播放模式为循环
+            movTexture.loop = false;
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
